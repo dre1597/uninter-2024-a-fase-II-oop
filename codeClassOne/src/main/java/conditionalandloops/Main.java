452ee37cb27ffe62c486c2c9bb5764073480fa13
André Pereira <andrepereira97@yahoo.com>
2024-04-13 00:13:15 -0300

    :sparkles: add some sout

--- codeClassOne/src/main/java/conditionalandloops/Main.java
+++ codeClassOne/src/main/java/conditionalandloops/Main.java
@@ -20,6 +20,14 @@ public class Main {
     int[] megasena1 = {11,34,43,22,45,17};
     int[] megasena2 = new int[6];
 
+    for (var i = 0; i < 6; i++) {
+      System.out.println(megasena1[i]);
+    }
+
+    for (var i = 0; i < 6; i++) {
+      System.out.println(megasena2[i]);
+    }
+
     try(var scanner = new Scanner(System.in)){
       System.out.print("Please write your name: ");
       name = scanner.nextLine();
