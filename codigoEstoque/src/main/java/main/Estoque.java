5f0f83a1c78bc68049f0a3d7babb85034fc6c57c
André Pereira <andrepereira97@yahoo.com>
2024-04-15 22:01:20 -0300

    :sparkles: add work example

---
+++ codigoEstoque/src/main/java/main/Estoque.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Estoque {
+  ArrayList<Computador> listaComputador = new ArrayList<Computador>();
+
+  public void adicionar(Computador c) {
+    listaComputador.add(c);
+  }
+
+  public void remover(Computador c) {
+    listaComputador.remove(c);
+  }
+
+  public void listar() {
+
+    for (Computador c : listaComputador) {
+      System.out.println(c);
+    }
+  }
+}
